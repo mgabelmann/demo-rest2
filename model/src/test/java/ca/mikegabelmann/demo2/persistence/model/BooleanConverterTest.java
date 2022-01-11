79ca081c00e604dc12097dae4a040ffe1a7fe5cd
mgabelmann <mgabelmann@gmail.com>
2022-01-10 19:50:24 -0800

    -removed unnecessary dependency in pom -added unit test to model

---
+++ model/src/test/java/ca/mikegabelmann/demo2/persistence/model/BooleanConverterTest.java
@@ -0,0 +1,34 @@
+package ca.mikegabelmann.demo2.persistence.model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+class BooleanConverterTest {
+    private BooleanConverter converter = new BooleanConverter();
+
+    @Test
+    @DisplayName("convert to database")
+    void convertToDatabaseColumn() {
+        Assertions.assertEquals(null, converter.convertToDatabaseColumn(null));
+        Assertions.assertEquals('Y', converter.convertToDatabaseColumn(Boolean.TRUE));
+        Assertions.assertEquals('N', converter.convertToDatabaseColumn(Boolean.FALSE));
+    }
+
+    @Test
+    @DisplayName("convert to entity")
+    void test1_convertToEntityAttribute() {
+        Assertions.assertEquals(null, converter.convertToEntityAttribute(null));
+        Assertions.assertEquals(Boolean.TRUE, converter.convertToEntityAttribute('Y'));
+        Assertions.assertEquals(Boolean.FALSE, converter.convertToEntityAttribute('N'));
+    }
+
+    @Test
+    @DisplayName("lowercase values are false")
+    void test2_convertToEntityAttribute() {
+        Assertions.assertEquals(Boolean.FALSE, converter.convertToEntityAttribute('y'));
+        Assertions.assertEquals(Boolean.FALSE, converter.convertToEntityAttribute('n'));
+    }
+
+}
