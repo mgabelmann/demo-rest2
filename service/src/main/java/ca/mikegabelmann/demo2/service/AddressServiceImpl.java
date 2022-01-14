236442797a35f51d29e28e98e59e3f22504ed11a
mgabelmann <mgabelmann@gmail.com>
2022-01-13 19:57:14 -0800

    -added some more tests and new method for address

--- service/src/main/java/ca/mikegabelmann/demo2/service/AddressServiceImpl.java
+++ service/src/main/java/ca/mikegabelmann/demo2/service/AddressServiceImpl.java
@@ -1,10 +1,16 @@
 package ca.mikegabelmann.demo2.service;
 
+import ca.mikegabelmann.demo2.dto.AddressDto;
+import ca.mikegabelmann.demo2.persistence.model.Address;
 import ca.mikegabelmann.demo2.persistence.repository.AddressRepository;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -18,6 +24,32 @@ public class AddressServiceImpl implements AddressService {
     @Autowired
     public AddressServiceImpl(final AddressRepository addressRepository) {
         this.addressRepository = addressRepository;
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<AddressDto> getAddressByCountryAndProvAndCity(String country, String prov, String city) {
+        List<Address> results = addressRepository.getAddressByCountryAndProvAndCity(country, prov, city);
+
+        return results.stream().map(AddressServiceImpl::map).collect(Collectors.toList());
+    }
+
+    /**
+     * Convert Address to AddressDto.
+     * @param a record
+     * @return mapped record
+     */
+    public static AddressDto map(Address a) {
+        AddressDto tmp;
+
+        if (a != null) {
+            tmp = new AddressDto(a.getId(), a.getAttention(), a.getStreetAddress(), a.getCity(), a.getProv(), a.getCountry(), a.getPostal());
+
+        } else {
+            tmp = null;
+        }
+
+        return tmp;
     }
 
 }
