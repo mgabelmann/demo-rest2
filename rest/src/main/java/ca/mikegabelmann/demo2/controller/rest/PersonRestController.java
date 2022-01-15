9169aa9cc76099e0cc8e5a15951e7be2fc4378cf
mgabelmann <mgabelmann@gmail.com>
2022-01-14 23:34:38 -0800

    removed unused import upgraded spring-boot-starter to 2.6.2, but spring-beans 5.3.12 is not compatible so I override temporarily until spring-boot-starter is upgraded/fixed.

--- rest/src/main/java/ca/mikegabelmann/demo2/controller/rest/PersonRestController.java
+++ rest/src/main/java/ca/mikegabelmann/demo2/controller/rest/PersonRestController.java
@@ -12,7 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
