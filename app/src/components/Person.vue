0f921133269d1ab423fa838b222662c262de6cc9
mgabelmann <mgabelmann@gmail.com>
2022-01-21 20:29:28 -0800

    added birth date and use internal object.

--- app/src/components/Person.vue
+++ app/src/components/Person.vue
@@ -7,20 +7,32 @@
       <div class="grid">
         <div class="col-6">
           <span class="p-float-label">
-            <InputText id="firstname" type="text" v-model="firstName"/>
+            <InputText id="firstname" type="text" v-model="person.firstName"/>
             <label for="firstname">First Name</label>
           </span>
         </div>
         <div class="col-6">
           <span class="p-float-label">
-            <InputText id="lastname" type="text" v-model="lastName"/>
+            <InputText id="lastname" type="text" v-model="person.lastName"/>
             <label for="lastname">Last Name</label>
           </span>
         </div>
         <div class="col-6">
           <span class="p-float-label">
-            <InputText id="middlename" type="text" v-model="middleName"/>
+            <InputText id="middlename" type="text" v-model="person.middleName"/>
             <label for="middlename">Middle Name</label>
+          </span>
+        </div>
+        <div class="col-6">
+          <span class="p-float-label">
+            <InputText id="birthdt" type="text" v-model="person.birthDt"/>
+            <label for="birthdt">Birth Date</label>
+          </span>
+        </div>
+        <div class="col-6">
+          <span class="p-float-label">
+            <InputText id="sex" type="text" v-model="person.sex"/>
+            <label for="sex">Sex</label>
           </span>
         </div>
       </div>
@@ -35,43 +47,46 @@ import axios from 'axios';
 
 export default {
   components: {
-    Card, InputText
+    Card,
+    InputText,
   },
   data() {
     return {
       loading: true,
 
-      personId: 'id',
-      firstName: 'Jane',
-      lastName: 'Doe',
-      middleName: '',
-      sex: 'M',
+      person: {
+        personId: '',
+        firstName: '',
+        lastName: '',
+        middleName: '',
+        birthDt: '',
+        sex: '',
+      },
     }
   },
-  methods() {
-
+  methods: {
+    async loadPerson() {
+      await axios.get('http://localhost:8080/persons/1')
+          .then(response => {
+            this.person = {...response.data};
+            this.person.middleName = response.data.middleName == null ? '' : response.data.middleName;
+            console.log("received person");
+          })
+          .catch(error => {
+            console.log(error)
+          })
+          .finally(() => this.loading = false);
+    },
   },
   computed: {
     fullName: function() {
-      return this.firstName + (this.middleName === '' ? ' ' : ' ' + this.middleName + ', ') + this.lastName;
+      return this.person.firstName + (this.person.middleName === '' ? ' ' : ' ' + this.person.middleName + ', ') + this.person.lastName;
     }
   },
   mounted() {
-    axios.get('http://localhost:8080/persons/1')
-        .then(response => {
-          this.personId = response.data.personId;
-          this.firstName = response.data.firstName;
-          this.lastName = response.data.lastName;
-          this.middleName = response.data.middleName == null ? '' : response.data.middleName;
-        })
-        .catch(error => {
-          console.log(error)
-        })
-        .finally(() => this.loading = false)
-  }
+    this.loadPerson();
+  },
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
