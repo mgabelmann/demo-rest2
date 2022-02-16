748d4b077a8408782e2f838a7b46ab9109ed57cf
mgabelmann <mgabelmann@gmail.com>
2022-02-15 18:56:43 -0800

    added configuration that loads based on the spring profile selected

---
+++ rest/src/main/java/ca/mikegabelmann/demo2/controller/config/LocalEnvironmentConfig.java
@@ -0,0 +1,25 @@
+package ca.mikegabelmann.demo2.controller.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.annotation.PostConstruct;
+
+
+@Profile("local")
+@Configuration
+public class LocalEnvironmentConfig {
+    /** Logger. */
+    private static final Logger LOG = LoggerFactory.getLogger(LocalEnvironmentConfig.class);
+
+
+    @PostConstruct
+    public void postConstruct() {
+        LOG.info("This config is only loaded if spring.profiles.active = local");
+    }
+
+    //TODO: add specialized beans here
+
+}
