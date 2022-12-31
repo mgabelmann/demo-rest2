527791845fd6a939b21fbb127bb6634eebc3c29f
mgabelmann <mgabelmann@gmail.com>
2022-12-31 11:09:07 -0800

    removed the @RestRepository interfaces since we dont need this anymore fixed broken integration test. something weird was happening. it was complaining that the @Repository beans were already registered, not sure why it is being scanned 2x

--- rest/src/test/java/ca/mikegabelmann/demo2/controller/rest/PersonRestControllerIT.java
+++ rest/src/test/java/ca/mikegabelmann/demo2/controller/rest/PersonRestControllerIT.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDate;
@@ -23,7 +24,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class PersonRestControllerIT {
+//FIXME: for some reason I need to include this otherwise I get an error stating that the Repository beans are already registered.
+@EnableJpaRepositories(basePackages = {
+    "ca.mikegabelmann.demo2.persistence.repository"
+})
+class PersonRestControllerIT {
     @Autowired
     private MockMvc mvc;
 
@@ -42,7 +47,7 @@ public class PersonRestControllerIT {
     void beforeEach() {
         this.now = LocalDate.now();
 
-        SexCode sTmp = ModelFactory.getSexCode_Male();
+        SexCode sTmp = ModelFactory.getSexCode_Female();
         this.s = sexCodeRepository.save(sTmp);
 
         Person pTmp = ModelFactory.getPerson(s);
